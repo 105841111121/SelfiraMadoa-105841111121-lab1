cc7d6597bec1007ffe44b03113b9e14b3c1e8b87
unknown <105841111121@student.unismuh.ac.id>
2024-07-07 23:58:26 +0800

    Text Input

---
+++ App.js
@@ -0,0 +1,37 @@
+import { Text, View, Image, TextInput} from 'react-native'
+import React from 'react'
+
+const App = () => {
+    const Inputan = ({ nama, color }) => {
+      return(
+        <TextInput
+        placeholder={`masukan ${nama}`}
+        style = {{
+          borderWidth: 1,
+          borderColor: 'black',
+          borderRadius: 10,
+          width: 300,
+          height: 50,
+          marginVertical: 10,
+          padding: 10,
+          backgroundColor: 'silver',
+          color: color,
+        }}
+      />
+      )
+    }
+
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+        <Inputan nama = " Email" color = " white"/>
+        <Inputan nama = "username" color = "white"/>
+        <Inputan nama = " pasword" color = "black"/>
+      </View>
+  )
+}
+
+export default App;
---
+++ component/button/pages/login.js
@@ -0,0 +1,17 @@
+import { text, View, Image, ScrollView } from 'react-native'
+import React from 'react'
+
+
+const login = () =>{
+    return(
+        <View style = {{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Text style = {{ color: 'white'}}>login</Text>
+        </View>
+    )
+       
+}
+export default login
